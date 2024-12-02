8cc408dc1faef2e2eb527cf1c602a7577b81086b
ertan usta <ertanusta@outlook.com>
2024-12-03 00:11:58 +0300

    son yapılanlar

--- src/Services/GoogleVertexAiService.php
+++ src/Services/GoogleVertexAiService.php
@@ -66,6 +66,7 @@ class GoogleVertexAiService
             return $cacheItem->get();
         }
         $configFilePath = $this->kernel->getProjectDir() . '/config/keys/falfal2.json';
+        var_dump($configFilePath);
         $client = new Client(); // Burasını bir parameters yaml a set edip getParameterBagden çağırmamız gerekiyor
         $client->useApplicationDefaultCredentials();
         $client->addScope('https://www.googleapis.com/auth/cloud-platform');
